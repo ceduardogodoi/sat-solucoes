556c6e43736008359d3a9662faeef23dbac5dfcd
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-01-22 18:07:01 -0300

    fix: removes confirm password validation

--- src/app/pessoas/create-pessoas/create-pessoas.component.ts
+++ src/app/pessoas/create-pessoas/create-pessoas.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup
-} from '@angular/forms';
-import { confirmaSenhaValidator } from '../../validators/confirmaSenha.validator';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import { rendaValidator } from '../../validators/renda.validator';
 
 @Component({
@@ -13,17 +8,12 @@ import { rendaValidator } from '../../validators/renda.validator';
   styleUrls: ['./create-pessoas.component.scss']
 })
 export class CreatePessoasComponent {
-  public personForm: FormGroup = this._formBuilder.group(
-    {
-      nome: [''],
-      dataCadastro: [''],
-      cpf: [''],
-      renda: ['', [rendaValidator()]],
-      senha: [''],
-      confirmarSenha: ['']
-    },
-    { validators: confirmaSenhaValidator }
-  );
+  public personForm: FormGroup = this._formBuilder.group({
+    nome: [''],
+    dataCadastro: [''],
+    cpf: [''],
+    renda: ['', [rendaValidator()]]
+  });
 
   public get controls(): { [key: string]: AbstractControl } {
     return this.personForm.controls;
@@ -32,14 +22,6 @@ export class CreatePessoasComponent {
   constructor(private _formBuilder: FormBuilder) {}
 
   public onSubmit(): void {
-    console.log('FormGroup errors:', this.personForm.errors);
-    console.log(
-      'FormControl senha errors:',
-      this.personForm.controls.senha.errors
-    );
-    console.log(
-      'FormControl confirmaSenha errors:',
-      this.personForm.controls.confirmarSenha.errors
-    );
+    console.log(this.personForm.value);
   }
 }
